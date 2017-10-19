4b0145ac8dcfb21d585572614ce3db7e002e9510
Yuchen Liu <liuyuchenzh@163.com>
2017-10-19 19:01:09 +0800

    first release

---
+++ README.md
@@ -0,0 +1,74 @@
+## Intro
+
+Simple tool to upload files to cdn
+
+## Environment requirement
+
+node >= 8
+
+## Install
+
+```bash
+npm install upload2cdn
+```
+
+## Notice
+
+This tool does not provide a service as uploading to cdn.<br>
+In fact, it actually depends on such service.<br>
+
+## Dependency
+
+`upload2cdn` relies on the existence a `cdn` object with an `upload` method described as below.
+
+```typescript
+type cdnUrl = string;
+interface cdnRes {
+  [localPath: string]: cdnUrl
+}
+// this is what cdn package looks like
+interface cdn {
+  upload: (localPaths: string[]) => Promise<cdnRes>;
+}
+```
+
+If typescript syntax is unfamiliar, here is another description in vanilla javascript.
+
+```js
+/**
+* @param {string[]} localPath: list of paths of local files
+* @return Promise<cdnRes>: resolved Promise with structure like {localPath: cdnUrl}
+*/
+function upload(localPath) {
+  // code
+}
+const cdn = {
+  upload
+}
+```
+
+## Usage
+
+
+```js
+const { upload } = require("upload2cdn")
+const cdn = require("some-cdn-package")
+  new upload(cdn, {
+      src: path.resolve("./src"), // where your html file would emit to (with reference to local js/css files)
+      dist: path.resolve('./dist'), // only use this when there is a need to separate origin outputs with cdn ones
+      assets: path.resolve('./src'), // where all assets lie, most likely the same as src property
+      urlCb(input) { return input }, // give the power to play with cdn url before emit
+      resolve: ['html'], // typeof file needed to match; default to ['html']
+      onFinish() {} // anything you want to run after the uploading and replacing process
+  })
+```
+
+> notice: src, dist, assets work best with absolute path!
+
+> `src` and `assets` are only two required
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2017-present, Yuchen Liu
---
+++ index.js
@@ -0,0 +1,292 @@
+const fs = require('fs')
+const fse = require('fs-extra')
+const path = require('path')
+const name = require('./package.json').name
+const DEFAULT_SEP = '/'
+const FILTER_OUT_DIR = [
+  '.idea',
+  '.vscode',
+  '.gitignore',
+  'node_modules',
+  '.DS_Store'
+]
+const ASSET_TYPE = [
+  'js',
+  'css',
+  'jpg',
+  'png',
+  'gif',
+  'svg',
+  'webp',
+  'woff',
+  'woff2',
+  'ttf',
+  'otf'
+]
+const DEFAULT_OPTION = {
+  src: resolve('src'),
+  dist: resolve('src'),
+  resolve: ['html'],
+  urlCb(input) {
+    return input
+  }
+}
+
+// 1. gather html file
+// 2. gather production file
+// 3. upload all production file
+// 4. find the usage of production file in html file
+// 5. if found, replace
+
+function resolve(...input) {
+  return path.resolve(...input)
+}
+
+function normalize(input, sep = DEFAULT_SEP) {
+  const _input = path.normalize(input)
+  return _input.split(path.sep).join(sep)
+}
+
+function isFilterOutDir(input) {
+  return FILTER_OUT_DIR.includes(input)
+}
+
+/**
+ * produce RegExp to match local path
+ * @param {string} localPath
+ * @return {RegExp}
+ */
+function generateLocalPathReg(localPath) {
+  const pathArr = localPath.split(DEFAULT_SEP)
+  const len = pathArr.length
+  const regStr = pathArr
+    .map((part, index) => {
+      if (index === len - 1) {
+        return `${part}`
+      } else {
+        return `\\.?(${part})?`
+      }
+    })
+    .join(`\\${DEFAULT_SEP}?`)
+  return new RegExp(regStr, 'g')
+}
+
+/**
+ * find file usage
+ * 1. make sure the range: srcPath
+ * 2. provide inline path to search and to replace with: localCdnPair
+ * @param {string} srcPath
+ * @param {string} distPath
+ * @return {function}
+ */
+function simpleReplace(srcPath, distPath = srcPath) {
+  const srcFile = fs.readFileSync(srcPath, 'utf-8')
+  return function savePair(localCdnPair) {
+    const ret = localCdnPair.reduce((last, file) => {
+      const localPath = normalize(file[0])
+      const cdnPath = file[1]
+      const localPathReg = generateLocalPathReg(localPath)
+      last = last.replace(localPathReg, match => cdnPath)
+      return last
+    }, srcFile)
+    fse.ensureFileSync(distPath)
+    fs.writeFileSync(distPath, ret)
+  }
+}
+
+/**
+ * gather specific file type within directory provided
+ * 1. provide range to search: src
+ * 2. provide the type of file to search: type
+ * @param {string} src: directory to search
+ * @return {function}
+ */
+function gatherFileIn(src) {
+  return function gatherFileType(type) {
+    return fs.readdirSync(src).reduce((last, file) => {
+      const filePath = resolve(src, file)
+      if (isFile(filePath)) {
+        path.extname(file) === `.${type}` && last.push(normalize(filePath))
+      } else if (isFilterOutDir(file)) {
+        // do nothing
+      } else if (isDir(filePath)) {
+        last = last.concat(gatherFileIn(filePath)(type))
+      }
+      return last
+    }, [])
+  }
+}
+
+function isFile(input) {
+  return fs.statSync(input).isFile()
+}
+
+function isDir(input) {
+  return fs.statSync(input).isDirectory()
+}
+
+function isType(type) {
+  return function enterFile(file) {
+    return isFile(file) && path.extname(file) === '.' + type
+  }
+}
+
+/**
+ * give the power of playing with cdn url
+ * @param {*[]} entries
+ * @param {function} cb
+ * @returns {[string, string][] | void}
+ */
+function processCdnUrl(entries, cb) {
+  if (typeof cb !== 'function')
+    return console.error(`[${name}]: urlCb is not function`)
+  return entries.map(pair => {
+    // pair[1] should be cdn url
+    pair[1] = cb(pair[1])
+    if (typeof pair[1] !== 'string')
+      console.error(`[${name}]: the return result of urlCb is not string`)
+    return pair
+  })
+}
+
+function mapSrcToDist(srcFilePath, srcRoot, distRoot) {
+  return srcFilePath.replace(srcRoot, distRoot)
+}
+
+const isJpg = isType('jpg')
+const isPng = isType('png')
+const isGif = isType('gif')
+const isWebp = isType('webp')
+const isCss = isType('css')
+const isJs = isType('js')
+const isWoff = isType('woff')
+const isWoff2 = isType('woff2')
+const isTtf = isType('ttf')
+const isOtf = isType('otf')
+const isSvg = isType('svg')
+
+function isFont(path) {
+  return (
+    isWoff(path) || isWoff2(path) || isTtf(path) || isOtf(path) || isSvg(path)
+  )
+}
+
+/**
+ * collect everything
+ * @param {function(string)}gatherFn
+ * @param {string[]} typeList
+ * @returns {{all: string[], js: string[], css: string[], img: string[], font: string[]}}
+ */
+function autoGatherFilesInAsset(gatherFn, typeList) {
+  return typeList.reduce(
+    (last, type) => {
+      const files = gatherFn(type)
+      if (!files.length) return last
+      const location = files[0]
+      last.all = last.all.concat(files)
+      if (
+        isGif(location) ||
+        isPng(location) ||
+        isJpg(location) ||
+        isWebp(location)
+      ) {
+        last.img = last.img.concat(files)
+      } else if (isCss(location)) {
+        last.css = last.css.concat(files)
+      } else if (isJs(location)) {
+        last.js = last.js.concat(files)
+      } else if (isFont(location)) {
+        last.font = last.font.concat(files)
+      }
+      return last
+    },
+    {
+      all: [],
+      img: [],
+      js: [],
+      font: [],
+      css: []
+    }
+  )
+}
+
+/**
+ * @typedef {function(string): string} urlCb
+ * @typedef {function(string[]): Promise<object>} uploadFn
+ */
+
+/**
+ * webpack upload plugin
+ * early version need more work
+ * custom cdn module, need to have an upload API, return a Promise with structured response
+ * like {localPath: cdnPath}
+ * @param {object} cdn
+ * @param {uploadFn} cdn.upload
+ * @param {object} option
+ * @param {string} option.src
+ * @param {string} option.assets
+ * @param {string=} option.dist
+ * @param {urlCb=} option.urlCb
+ * @param {function=} option.onFinish
+ * provide information about what the source html directory and compiled html directory
+ */
+
+async function upload(cdn, option) {
+  console.log(`[${name}]: start...`)
+  const $option = Object.assign({}, DEFAULT_OPTION, option)
+  // extra treatment for cdnUrl
+  const urlCb = $option.urlCb
+  // could process other type of files rather than limited to html
+  const resolveList = $option.resolve
+  // get absolute path of src and dist directory
+  const srcRoot = resolve($option.src)
+  const distRoot = resolve($option.dist)
+  // onFinish callback
+  const onFinish = $option.onFinish
+
+  // all assets including js/css/img
+  const assets = resolve($option.assets)
+  const gatherFileInAssets = gatherFileIn(assets)
+
+  const assetsFiles = autoGatherFilesInAsset(gatherFileInAssets, ASSET_TYPE)
+
+  const { img, css, js, font } = assetsFiles
+
+  // upload img/font
+  // find img/font in css
+  // replace css
+  // now css ref to img/font with cdn path
+  // meanwhile upload chunk files to save time
+  console.log(`[${name}]: uploading img + font ...`)
+  const imgAndFontPairs = await cdn.upload([...img, ...font])
+
+  // update css files with cdn img/font
+  css.forEach(name => {
+    simpleReplace(name)(processCdnUrl(Object.entries(imgAndFontPairs), urlCb))
+  })
+
+  // concat js + css + img
+  console.log(`[${name}]: uploading js + css`)
+  const adjustedFiles = [...js, ...css, ...img]
+  const findFileInRoot = gatherFileIn($option.src)
+  const tplFiles = resolveList.reduce((last, type) => {
+    last = last.concat(findFileInRoot(type))
+    return last
+  }, [])
+  const jsCssImgPair = await cdn.upload(adjustedFiles)
+  const localCdnPair = Object.entries(jsCssImgPair)
+  tplFiles.forEach(filePath => {
+    simpleReplace(filePath, mapSrcToDist(filePath, srcRoot, distRoot))(
+      processCdnUrl(localCdnPair, urlCb)
+    )
+  })
+  // run onFinish if it is a valid function
+  onFinish && typeof onFinish === 'function' && onFinish()
+  console.log(`[${name}]: all done`)
+}
+
+module.exports = {
+  upload,
+  gatherFileIn,
+  autoGatherFilesInAsset
+}
